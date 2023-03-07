f61c32aa2705392cf212155f219a77c7e126af75
Azdev17 <jonathan.duret17@aol.com>
2023-03-07 15:26:51 +0100

    Initial Commit

---
+++ Learning GItHub/Learning_GItHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GItHubApp.swift
+//  Learning GItHub
+//
+//  Created by Jonathan DURET on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GItHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
