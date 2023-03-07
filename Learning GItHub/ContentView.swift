e532b47a9f55469b4480c8745dd2ed190cba157a
azdev17 <92388064+azdev17@users.noreply.github.com>
2023-03-07 19:21:28 +0100

    Update ContentView to What's up !

--- Learning GItHub/ContentView.swift
+++ Learning GItHub/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Japan!")
+            Text("What's up !, Japan!")
         }
         .padding()
     }
